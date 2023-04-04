59954b6f3bdc4579cc2e848fb96b330ab082c5a0
LautaroCavichia <lautarocavichia@gmail.com>
2023-04-04 12:17:00 +0200

    Created activity log class and .cpp/.h files, method for storing activities in a vector and the possibility to search by date.

---
+++ activity_log.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Lautaro Cavichia on 04/04/23.
+//
+
+#include "activity_log.h"
+
+void ActivityLog::addActivity(const Activity &activity) {
+    this->activities.push_back(activity);
+}
+
+Activity &ActivityLog::getActivity(int index) {
+    return this->activities[index];
+}
+
+void ActivityLog::removeActivity(int index) {
+    this->activities.erase(this->activities.begin() + index);
+}
+
+vector<Activity> ActivityLog::searchByDate(const string &dateString) const {
+    vector<Activity> activitiesOnDate;
+    for (const Activity& activity : this->activities) { // For each activity in the activities vector
+        if (activity.getDateString().find(dateString) != string::npos) {  // If the date string is found in the activity date string. npos means not found in the string
+            activitiesOnDate.push_back(activity); // Add the activity to the vector
+        }
+    }
+    return activitiesOnDate; //FIXME: Return a reference to the vector? is possible?
+}
---
+++ activity_log.h
@@ -0,0 +1,30 @@
+//
+// Created by Lautaro Cavichia on 04/04/23.
+//
+
+#ifndef ACTIVITYTRACKER_ACTIVITY_LOG_H
+#define ACTIVITYTRACKER_ACTIVITY_LOG_H
+
+#include <vector>
+#include <string>
+#include "activity.h"
+
+using namespace std;
+
+class ActivityLog{
+public:
+    ActivityLog(): activities(vector<Activity>()){}
+    void addActivity(const Activity &activity);
+    void removeActivity(int index);
+    Activity &getActivity(int index); // Returns a reference to the activity at the given index
+    vector<Activity> searchByDate(const string &dateString)const; // Returns a vector of activities that match the date (Project requirement)
+
+    //TODO: Add sorting methods and log to file methods
+
+private:
+    vector<Activity> activities;
+};
+
+
+
+#endif //ACTIVITYTRACKER_ACTIVITY_LOG_H
