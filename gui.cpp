63b7f9fb81173b76f076cd394248373554653af2
LautaroCavichia <lautarocavichia@gmail.com>
2023-04-06 17:39:54 +0200

    GUI v0.2 , Improved functionality, now writing events as a list.

--- gui.cpp
+++ gui.cpp
@@ -7,8 +7,8 @@
 
 enum IDs {
     ID_AddActivity = 100,
-    ID_Hours = 101,
-    ID_Minutes = 102,
+    ID_StartTime = 101,
+    ID_EndTime = 102,
     ID_Date = 103,
     ID_ActivityName = 110,
     ID_Description = 111,
@@ -19,14 +19,14 @@ enum IDs {
 
 wxBEGIN_EVENT_TABLE(GUI, wxFrame)
                 EVT_BUTTON(ID_AddActivity, GUI::OnAddActivityButton)
-                EVT_SPINCTRL(ID_Hours, GUI::OnSelectHour)
-                EVT_SPINCTRL(ID_Minutes, GUI::OnSelectMinute)
+                EVT_TIME_CHANGED(ID_StartTime, GUI::OnSelectStartTime)
+                EVT_TIME_CHANGED(ID_EndTime, GUI::OnSelectEndTime)
                 EVT_DATE_CHANGED(ID_Date, GUI::OnSelectDate)
                 EVT_TEXT(ID_ActivityName, GUI::OnActivityName)
                 EVT_TEXT(ID_Description, GUI::OnAddDescription)
 wxEND_EVENT_TABLE()
 
-GUI::GUI(const wxString &title, const wxPoint &pos, const wxSize &size): wxFrame(nullptr, wxID_ANY, title, pos, size) {  //TODO center the widgets
+GUI::GUI(const wxString &title, const wxPoint &pos, const wxSize &size): wxFrame(nullptr, wxID_ANY, title, pos, size) {
 
     this->SetSizeHints( wxDefaultSize, wxDefaultSize );
 
@@ -49,16 +49,18 @@ GUI::GUI(const wxString &title, const wxPoint &pos, const wxSize &size): wxFrame
     wxBoxSizer* bSizer4;
     bSizer4 = new wxBoxSizer( wxHORIZONTAL );
 
-    m_spinCtrl1 = new wxSpinCtrl( this, ID_Hours, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0, 24, 8 );
-    bSizer4->Add( m_spinCtrl1, 0, wxALIGN_CENTER, 5 );
+   // m_spinCtrl1 = new wxSpinCtrl( this, ID_Hours, wxString::Format("%d", defaultHourValue), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0, 24, defaultHourValue ); //defaultHoursValue as a string defaultHoursValue
+    m_startTime = new wxTimePickerCtrl(this, ID_StartTime, wxDateTime::Now().GetHour(), wxDefaultPosition, wxDefaultSize, wxTP_DEFAULT); //TODO eliminate seconds
+    bSizer4->Add( m_startTime, 0, wxALIGN_CENTER, 5 );
 
-    m_spinCtrl2 = new wxSpinCtrl( this, ID_Minutes, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0, 59, 0 );
-    bSizer4->Add( m_spinCtrl2, 0, wxALIGN_CENTER, 5 );
+   // m_spinCtrl2 = new wxSpinCtrl( this, ID_Minutes, wxString::Format("%02d", defaultMinuteValue), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0, 59, defaultMinuteValue ); //defaultMinutesValue as a string defaultM
+    m_endTime = new wxTimePickerCtrl(this, ID_EndTime, wxDateTime::Now().Add(wxTimeSpan::Hours(1)).GetHour(), wxDefaultPosition, wxDefaultSize, wxTP_DEFAULT); //TODO eliminate seconds
+    bSizer4->Add( m_endTime, 0, wxALIGN_CENTER, 5 );
 
 
     bSizer5->Add( bSizer4, 1, wxALIGN_CENTER|wxALL, 10 );
 
-    m_datePicker1 = new wxDatePickerCtrl( this, ID_Date, wxDefaultDateTime, wxDefaultPosition, wxDefaultSize, wxDP_DEFAULT );
+    m_datePicker1 = new wxDatePickerCtrl( this, ID_Date, wxDateTime::Today(), wxDefaultPosition, wxDefaultSize, wxDP_DEFAULT ); //today as default date
     bSizer5->Add( m_datePicker1, 0, wxALIGN_CENTER|wxALL, 10 );
 
     m_textCtrl1 = new wxTextCtrl( this, ID_ActivityName, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
@@ -75,15 +77,11 @@ GUI::GUI(const wxString &title, const wxPoint &pos, const wxSize &size): wxFrame
 
     bSizer8->Add( bSizer6, 1, wxALIGN_CENTER|wxALL, 5 );
 
-    m_simplebook1 = new wxSimplebook( this, ID_ActivityList, wxDefaultPosition, wxDefaultSize, 0 );
-    m_simplebook1->SetBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_CAPTIONTEXT ) );
-    m_simplebook1->SetMinSize( wxSize( 200,200 ) );
+    m_notebook1 = new wxNotebook( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0 );
+    m_notebookPage1 = new wxTextCtrl( m_notebook1, ID_ActivityList, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE );
 
-    m_scrolledWindow1 = new wxScrolledWindow( m_simplebook1, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxHSCROLL|wxVSCROLL );
-    m_scrolledWindow1->SetScrollRate( 5, 5 );
-    m_simplebook1->AddPage( m_scrolledWindow1, wxT("a page"), false );
-
-    bSizer8->Add( m_simplebook1, 1, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+    m_notebook1->AddPage( m_notebookPage1, wxT("Activities"), true );
+    bSizer8->Add( m_notebook1, 1, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
 
 
     bSizer1->Add( bSizer8, 1, wxALIGN_CENTER, 5 );
@@ -112,34 +110,34 @@ void GUI::test(wxCommandEvent &event) {
     wxMessageBox("Hello World", "Test", wxOK | wxICON_INFORMATION);
 }
 
-void GUI::OnSelectHour(wxSpinEvent &event) {
-    wxString hour = wxString::Format(wxT("%d"), event.GetPosition());
-    wxLogStatus(hour);
+void GUI::OnSelectStartTime(wxDateEvent &event) {
+    activityStartTime = event.GetDate();
+    formattedActivityStartTime = activityStartTime.Format("%H:%M");
 }
 
-void GUI::OnSelectMinute(wxSpinEvent &event) {
-    wxString minute = wxString::Format(wxT("%d"), event.GetPosition());
-    wxLogStatus(minute);
+void GUI::OnSelectEndTime(wxDateEvent &event) {
+    activityEndTime = event.GetDate();
+    formattedActivityEndTime = activityEndTime.Format("%H:%M");
 }
 
 void GUI::OnSelectDate(wxDateEvent &event) {
-    wxDateTime date = event.GetDate();
-    wxLogStatus(date.Format("%d/%m/%Y"));
+    activityDate = event.GetDate();
+    formattedDate = activityDate.Format("%d/%m/%Y");
 }
 
 void GUI::OnActivityName(wxCommandEvent &event) {
-    wxString name = event.GetString();
-    wxLogStatus(name);
+    activityName = event.GetString();
 }
 
 void GUI::OnAddDescription(wxCommandEvent &event) {
-    wxString description = event.GetString();
-    wxLogStatus(description);
+    activityDescription = event.GetString();
 }
 
 void GUI::OnAddActivityButton(wxCommandEvent &event) {
 
-    wxLogStatus("Activity added");
+    wxLogStatus("Activity " + activityName + " added at " + formattedActivityStartTime + " to " + formattedActivityEndTime + " on " + formattedDate);
+    m_notebookPage1->AppendText(activityName + " - " + formattedActivityStartTime + " - " + formattedActivityEndTime + " on " + formattedDate + "\n");
+
 
 }
 
--- gui.h
+++ gui.h
@@ -13,6 +13,7 @@
 #include <wx/textctrl.h>
 #include <wx/simplebook.h>
 #include <string>
+#include <wx/timectrl.h>
 
 
 
@@ -27,21 +28,33 @@ private:
     wxPanel *panel; // A panel is a container for widgets
     void OnExit(wxCommandEvent &event);
     void test(wxCommandEvent &event);
-    void OnSelectHour(wxSpinEvent &event);
-    void OnSelectMinute(wxSpinEvent &event);
+    void OnSelectStartTime(wxDateEvent &event);
+    void OnSelectEndTime(wxDateEvent &event);
     void OnSelectDate(wxDateEvent &event);
     void OnActivityName(wxCommandEvent &event);
     void OnAddDescription(wxCommandEvent &event);
     void OnAddActivityButton(wxCommandEvent &event);
 
+    wxString activityName;
+    wxString activityDescription;
+    wxDateTime activityDate;
+    wxDateTime activityStartTime;
+    wxDateTime activityEndTime;
+    wxString formattedActivityStartTime = wxDateTime::Now().Format(wxT("%H:00")); //Now as default value
+    wxString formattedActivityEndTime = wxDateTime::Now().Add(wxTimeSpan::Hours(1)).Format(wxT("%H:00")); //Now + one hour as default value
+    wxString formattedDate = wxDateTime::Now().Format("%d/%m/%Y"); //Now as default value
+
 protected:
     wxStaticText* m_staticText1;
-    wxSpinCtrl* m_spinCtrl1;
-    wxSpinCtrl* m_spinCtrl2;
+   // wxSpinCtrl* m_spinCtrl1;
+  //  wxSpinCtrl* m_spinCtrl2;
+    wxTimePickerCtrl *m_startTime;
+    wxTimePickerCtrl *m_endTime;
     wxDatePickerCtrl* m_datePicker1;
     wxTextCtrl* m_textCtrl1;
     wxButton* m_button1;
-    wxSimplebook* m_simplebook1;
+    wxNotebook* m_notebook1;
+    wxTextCtrl* m_notebookPage1;
     wxScrolledWindow* m_scrolledWindow1;
     wxToolBar* m_toolBar2;
     wxToolBarToolBase* m_tool1;
