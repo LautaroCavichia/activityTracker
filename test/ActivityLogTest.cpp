3b565e3969cca9fd45d912790426de049252d6f1
LautaroCavichia <lautarocavichia@gmail.com>
2023-07-21 17:49:08 +0200

    added editActivity method to unitTesting

--- test/ActivityLogTest.cpp
+++ test/ActivityLogTest.cpp
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 #include "../activity.h"
 #include "../activity_log.h"
+#include "../TimeUtilities.h"
 
 TEST(logTest, vectorSize1)
 {
@@ -126,4 +127,78 @@ TEST(logTest, searchOutOfRange){
     ASSERT_THROW(log.getActivity(-1), IndexException);
     ASSERT_THROW(log.getActivity(19), IndexException);
 
+}
+
+TEST(logTest, editTest){
+    ActivityLog log;
+    Activity activity("Prova1", "08/06/2023 09:45:00", "08/06/2023 09:45:00", "Questa è una prova");
+    log.addActivity(activity);
+
+    ASSERT_EQ(log.searchByDate("08/06/2023").size(), 1);
+
+    Activity tempActivity("temp", "08/06/2023 10:45:00", "08/06/2023 11:45:00", "temp2");
+    log.editActivity(0,tempActivity);
+
+    ASSERT_EQ(log.searchByDate("08/06/2023").size(), 1);
+    ASSERT_EQ(log.getActivity(0).getName(), "temp");
+    ASSERT_EQ(log.getActivity(0).getDescription(), "temp2");
+    ASSERT_EQ(TimeUtilities::timePointToString(log.getActivity(0).getStartTime()), "10:45");
+    ASSERT_EQ(TimeUtilities::timePointToString(log.getActivity(0).getEndTime()), "11:45");
+}
+
+TEST(logTest, editingMultipleTimesTest){
+    ActivityLog log;
+    Activity activity("Prova1", "08/06/2023 09:45:00", "08/06/2023 09:45:00", "Questa è una prova");
+    log.addActivity(activity);
+
+    ASSERT_EQ(log.searchByDate("08/06/2023").size(), 1);
+
+    Activity tempActivity("temp", "08/06/2023 10:45:00", "08/06/2023 11:45:00", "temp2");
+    log.editActivity(0,tempActivity);
+
+    ASSERT_EQ(log.searchByDate("08/06/2023").size(), 1);
+    ASSERT_EQ(log.getActivity(0).getName(), "temp");
+    ASSERT_EQ(log.getActivity(0).getDescription(), "temp2");
+    ASSERT_EQ(TimeUtilities::timePointToString(log.getActivity(0).getStartTime()), "10:45");
+    ASSERT_EQ(TimeUtilities::timePointToString(log.getActivity(0).getEndTime()), "11:45");
+
+    Activity tempActivity2("temp4", "08/06/2023 12:45:00", "08/06/2023 13:45:00", "temp5");
+    log.editActivity(0,tempActivity2);
+
+    ASSERT_EQ(log.searchByDate("08/06/2023").size(), 1);
+    ASSERT_EQ(log.getActivity(0).getName(), "temp4");
+    ASSERT_EQ(log.getActivity(0).getDescription(), "temp5");
+    ASSERT_EQ(TimeUtilities::timePointToString(log.getActivity(0).getStartTime()), "12:45");
+    ASSERT_EQ(TimeUtilities::timePointToString(log.getActivity(0).getEndTime()), "13:45");
+}
+TEST(logTest, editingNotTheFirstActivity){
+    ActivityLog log;
+    Activity activity("Prova1", "08/06/2023 09:45:00", "08/06/2023 09:45:00", "Questa è una prova");
+    Activity activity2("Prova2", "08/06/2023 09:45:00", "08/06/2023 09:45:00", "Questa è una prova");
+    log.addActivity(activity);
+    log.addActivity(activity2);
+
+    ASSERT_EQ(log.searchByDate("08/06/2023").size(), 2);
+
+    Activity tempActivity("temp", "08/06/2023 10:45:00", "08/06/2023 11:45:00", "temp2");
+    log.editActivity(1,tempActivity);
+
+    ASSERT_EQ(log.searchByDate("08/06/2023").size(), 2);
+    ASSERT_EQ(log.getActivity(1).getName(), "temp");
+    ASSERT_EQ(log.getActivity(1).getDescription(), "temp2");
+    ASSERT_EQ(TimeUtilities::timePointToString(log.getActivity(1).getStartTime()), "10:45");
+    ASSERT_EQ(TimeUtilities::timePointToString(log.getActivity(1).getEndTime()), "11:45");
+}
+
+TEST(logTest, editingOutOfBoundIndex) {
+    ActivityLog log;
+    Activity activity("Prova1", "08/06/2023 09:45:00", "08/06/2023 10:17:00", "Questa è una prova");
+    log.addActivity(activity);
+
+    ASSERT_EQ(log.searchByDate("08/06/2023").size(), 1);
+
+    ASSERT_THROW(log.editActivity(-1,activity), IndexException);
+    ASSERT_THROW(log.editActivity(19,activity),IndexException);
+
+    ASSERT_EQ(log.searchByDate("08/06/2023").size(), 1);
 }
